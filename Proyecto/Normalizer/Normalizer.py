f72f382139ce8d088d7a991fba2e48a0f72ada98
Unknown <jkherrera96@gmail.com>
2018-10-18 12:42:33 -0600

    P1- comments to a-score

--- Proyecto/Normalizer/Normalizer.py
+++ Proyecto/Normalizer/Normalizer.py
@@ -31,7 +31,7 @@ class Normalizer():
         one_hot = pd.get_dummies( current_column[ column_name ])
         return one_hot  
     
-    # ------------------------------------------------------------
+    # -----------------------------------------------------------
     # input: none
     # function: 
     # output:         
@@ -103,6 +103,10 @@ class Normalizer():
 
     #  --------------Z score-----------------------------------
 
+    # -----------------------------------------------------------
+    # input: int: media , list of x values
+    # function: sum all square of  x- media
+    # output: int: sum
     def square_sum(self, media, Xs):
         sum = 0
         for i in range(len(Xs)):
@@ -111,6 +115,10 @@ class Normalizer():
             sum = sum + temp
         return sum
 
+    # -----------------------------------------------------------
+    # input: int: n, int: media, list of x values
+    # function: calculate standar desviation
+    # output: number with the standar desviation
     def stand_deviation(self, n, media, Xs):
 
         square_sum = self.square_sum(media, Xs)
@@ -119,6 +127,10 @@ class Normalizer():
 
         return sqrt
 
+    # -----------------------------------------------------------
+    # input: list of x values, int: media, int: standar desviation
+    # function: do z score formula
+    # output: list with the scores
     def z_score_formula(self, Xs, media, stand_desviation):
 
         scores = []
@@ -128,16 +140,26 @@ class Normalizer():
             scores += [[result]]
         return scores
 
+    # -----------------------------------------------------------
+    # input: list of scores, string: name of column
+    # function: create a dataframe column
+    # output: pandas dataframe
     def zs_column(self, data, name):
 
         df = pd.DataFrame(data, columns=[name])
         return df
-
+    # -----------------------------------------------------------
+    # input: a pandas series
+    # function: convert a pandas series in a int
+    # output: a number
     def series_toInt(self, pd_series):
         ltemp = pd_series.tolist()
         return ltemp[0]
 
-
+    # -----------------------------------------------------------
+    # input: pandas dataframes, string: column name
+    # function: do the calculate of s-score
+    # output: pandas dataframe
     def z_score(self, data_set, column_name):
         sum = self.series_toInt(data_set.sum())
         n = data_set.shape[0]
@@ -150,4 +172,4 @@ class Normalizer():
         return df
 
 
-    # --------------One hot---------------------------
+    # ----------------------One hot---------------------------

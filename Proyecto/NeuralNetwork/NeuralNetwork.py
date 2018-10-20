c5d56d36ea56ebd2ae33755538c2ef8ee9a0e981
Anthony Leandro <Anthonylle@hotmail.com>
2018-10-20 15:54:29 -0600

    Merge from master
    Changelog:
    * Net class tests file created 
    * Console argparse main program configurated
    * Some NeuralNetwork class settings changed

--- Proyecto/NeuralNetwork/NeuralNetwork.py
+++ Proyecto/NeuralNetwork/NeuralNetwork.py
@@ -1,4 +1,4 @@
-from tensorflow.nn import relu, sigmoid, softmax
+from tensorflow.nn import relu, sigmoid, softmax, softplus
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.losses import MSE, binary_crossentropy
@@ -19,10 +19,12 @@ class NeuralNetwork(Model):
     y_train_data = None
     x_test_data = None
     y_test_data = None
+    x_val_data = None
+    y_val_data = None
     model = None
 
     def __init__(self):
-        pass
+        self.model = Sequential()
 
     def load_data(self):
         self.input_vars = DataFrame()
@@ -38,37 +40,46 @@ class NeuralNetwork(Model):
         data = self.output_var.data_set
         self.output_var.data_set = normalizer.normalize_data(data)
         '''
-        print(self.output_var.data_set)
-        print(self.input_vars.data_set)
+        print("---- Output vars ---- \n", self.output_var.data_set)
+        print("---- Input vars ---- \n", self.input_vars.data_set)
         '''
 
     def assign_data(self):
-        self.x_train_data = self.input_vars.sub_data_set(0,400).values
-        # print("X_train_data: ", self.x_train_data)
-        self.y_train_data = self.output_var.sub_data_set(0,400).values
-        self.x_test_data = self.input_vars.sub_data_set(400,569).values
-        self.y_test_data = self.output_var.sub_data_set(400, 569).values
+        self.x_train_data = self.input_vars.sub_data_set(0,200).values
+        self.y_train_data = self.output_var.sub_data_set(0,200).values
+
+        self.x_val_data = self.input_vars.sub_data_set(201,400).values
+        self.y_val_data = self.output_var.sub_data_set(201,400).values
+
+        self.x_test_data = self.input_vars.sub_data_set(401,569).values
+        self.y_test_data = self.output_var.sub_data_set(401, 569).values
 
     def create_model(self, kwargs):
-        self.model = Sequential()
-        self.model.compile(optimizer=Adamax(), loss=MSE)
-        no_units = 2 # number of units (neurons) in a layer
-        # number of columns in the input data to train ToDo x_train_data
-        # ToDo create loop for adding new layers to the model
-        self.model.add(Dense(no_units, activation=relu))
+        self.model.compile(optimizer=SGD(), loss=MSE)
+
+        no_units = kwargs["units"]  # number of units (neurons) in a layer
+        available_activation_functions = {"relu": relu, "sigmoid": sigmoid, "softmax": softmax, "softplus": softplus}
+        activation_function = available_activation_functions[kwargs["activation"]]
+
+        for i in range(0, kwargs["layers"]):
+            self.model.add(Dense(no_units, activation=activation_function))
+        self.load_data()
+        self.normalize_data()
+        self.assign_data()
 
     def train_model(self):
-        self.model.fit(self.x_train_data,self.y_train_data, validation_data=(self.x_test_data, self.y_test_data), batch_size=57)
+        self.model.fit(self.x_train_data,self.y_train_data, validation_data=(self.x_val_data, self.y_val_data), batch_size=57)
 
     def evaluate_model(self):
-        pass
+        return self.model.evaluate_model(self.x_test_data, self.y_test_data)
+
 
 
 instance = NeuralNetwork()
 instance.load_data()
 instance.normalize_data()
 instance.assign_data()
-instance.create_model(kwargs=None)
+instance.create_model(kwargs={"layers": 5, "units": 2, "activation":"softmax"})
 instance.train_model()
 accuracy = instance.model.evaluate(instance.x_test_data, instance.y_test_data)
 print("Accuracy: ", accuracy)
--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,7 +1,37 @@
-from pruebas import normalizer_data
+'''from ..pruebas import normalizer_data
 
 def main():
     normalizer_data()
     
 if __name__ == "__main__":
-    main()    
+    main()    '''
+from Proyecto.NeuralNetwork.NeuralNetwork import NeuralNetwork
+import argparse
+
+parser = argparse.ArgumentParser(description="Programa de predicción de datos utilizando Random Forests o Redes Neuronales", epilog="Eso es todo amigos")
+
+forests_group = parser.add_argument_group("Random Forests")
+net_group = parser.add_argument_group("Red Neuronal")
+
+forests_group.add_argument("--arbol", help="El modelo por utilizar será: Random Forests", action="store_true")
+forests_group.add_argument("--umbral-poda", type=float, default=0.5, metavar='', help="Ganancia mínima requerida para realizar una partición")
+
+net_group.add_argument("--red-neuronal", help="El modelo por utilizar será: Red Neuronal", action="store_true")
+net_group.add_argument("--numero-capas", type=int, default=1, metavar='', help="Cantidad de capas creadas en la Red Neuronal")
+net_group.add_argument("--unidades-por-capa", type=int, default=1, metavar='', help="Cantidad de neuronas por capa en la Red Neuronal")
+net_group.add_argument("--funcion-activacion", type=str, default='relu', metavar='', help='Define la salida de los nodos en una Red Neuronal para un conjunto de entradas dado')
+
+args = parser.parse_args()
+
+if args.arbol:
+    print("Arbol has been chosen")
+
+
+elif args.red_neuronal:
+    model = NeuralNetwork()
+    model.create_model(kwargs={"layers": args.numero_capas, "units": args.unidades_por_capa, "activation": args.funcion_activacion})
+    model.train_model()
+    print(model.evaluate_model())
+
+else:
+    print("Debe elegir --arbol o sino --red-neuronal")

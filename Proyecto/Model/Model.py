61572178780462d2a8acda04fb534ab54ea15d40
Anthony Leandro <Anthonylle@hotmail.com>
2018-10-20 20:27:26 -0600

    Checkpoint

--- Proyecto/Model/Model.py
+++ Proyecto/Model/Model.py
@@ -8,14 +8,20 @@ class Model(metaclass=ABCMeta):
         pass
 
     def create_model(self, *kwargs):
-        '''
-        :param args:
+        """
+        :param kwargs: specific configuration options to create the model
         :return:
-        '''
+        """
         pass
 
-    def train_model(self, x, y):
+    def train_model(self, x_train_data, y_train_data):
+        """
+        :param x_train_data: x values to train the model
+        :param y_train_data: the objective or goal for the training process
+        :return:
+        """
+
         pass
 
-    def evaluate_model(self, x, y):
+    def evaluate_model(self, x_test_data, y_test_data):
         pass
--- Proyecto/NeuralNetwork/NeuralNetwork.py
+++ Proyecto/NeuralNetwork/NeuralNetwork.py
@@ -1,58 +1,19 @@
 from tensorflow.nn import relu, sigmoid, softmax, softplus
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-from tensorflow.keras.losses import MSE, binary_crossentropy
+from tensorflow.keras.losses import MSE
 from tensorflow.keras.optimizers import Adamax, Adam, SGD
-from Proyecto.DataSet.DataFrame import DataFrame
-from Proyecto.Normalizer.Normalizer import Normalizer
-from Proyecto.Model.Model import Model
+from Model.Model import Model
 import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 class NeuralNetwork(Model):
-    input_vars = None
-    output_var = None
-    normalizer = None
-    x_train_data = None
-    y_train_data = None
-    x_test_data = None
-    y_test_data = None
-    x_val_data = None
-    y_val_data = None
     model = None
 
     def __init__(self):
         self.model = Sequential()
-
-    def load_data(self):
-        self.input_vars = DataFrame()
-        self.input_vars.load_data_set("breast-cancer-wisconsin-data.csv")
-        self.input_vars.drop_columns_by_name(['id', 'Unnamed: 32'])
-        self.output_var = DataFrame()
-        self.output_var.data_set = self.input_vars.cut_column('diagnosis')
-
-    def normalize_data(self):
-        normalizer = Normalizer()
-        data = self.input_vars.data_set
-        self.input_vars.data_set = normalizer.normalize_data(data)
-        data = self.output_var.data_set
-        self.output_var.data_set = normalizer.normalize_data(data)
-        '''
-        print("---- Output vars ---- \n", self.output_var.data_set)
-        print("---- Input vars ---- \n", self.input_vars.data_set)
-        '''
-
-    def assign_data(self):
-        self.x_train_data = self.input_vars.sub_data_set(0,200).values
-        self.y_train_data = self.output_var.sub_data_set(0,200).values
-
-        self.x_val_data = self.input_vars.sub_data_set(201,400).values
-        self.y_val_data = self.output_var.sub_data_set(201,400).values
-
-        self.x_test_data = self.input_vars.sub_data_set(401,569).values
-        self.y_test_data = self.output_var.sub_data_set(401, 569).values
 
     def create_model(self, kwargs):
         self.model.compile(optimizer=SGD(), loss=MSE)
@@ -63,15 +24,12 @@ class NeuralNetwork(Model):
 
         for i in range(0, kwargs["layers"]):
             self.model.add(Dense(no_units, activation=activation_function))
-        self.load_data()
-        self.normalize_data()
-        self.assign_data()
 
-    def train_model(self):
-        self.model.fit(self.x_train_data,self.y_train_data, validation_data=(self.x_val_data, self.y_val_data), batch_size=57)
+    def train_model(self, x_train_data, y_train_data):
+        self.model.fit(x_train_data, y_train_data)  #  , validation_data=(self.x_val_data, self.y_val_data), batch_size=57)
 
-    def evaluate_model(self):
-        return self.model.evaluate_model(self.x_test_data, self.y_test_data)
+    def evaluate_model(self, x_test_data, y_test_data):
+        return self.model.evaluate_model(x_test_data, y_test_data)
 
 
 
